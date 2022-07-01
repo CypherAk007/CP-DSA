ffe1bc74b2ab5602e5d57eef6854983d4850cbb1
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-01 18:15:55 +0530

    Time: 87 ms (16.25%), Space: 14.3 MB (99.32%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,16 +1,25 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:        
-        n=len(matrix)
-        m=len(matrix[0])
-        low=0
-        high=(n*m)-1
-        # matrix[i][j] i=mid//no.col ; j=mid%no.col
-        while low<=high:
-            mid=(low+high)//2
-            if target>matrix[mid//m][mid%m]:
-                low=mid+1
-            elif matrix[mid//m][mid%m]>target:
-                high=mid-1
-            else:
+    def searchMatrix(self, matrix: List[List[int]], t: int) -> bool:        
+        i=self.findRow(matrix,t,len(matrix[0]))
+        # print(i)
+        return self.binarySearch(matrix[i],0,len(matrix[0])-1,t)
+    
+    def findRow(self,matrix,t,last):
+        for i in range(len(matrix)):
+            if t<=matrix[i][last-1]:
+                # print(i)
+                return i
+        return -1
+            
+    def binarySearch(self,a,l,h,t):
+        # print(a,l,h,t)
+        while(l<=h):
+            m=(l+h)//2
+            if t==a[m]:
+                # print(m,a[m])
                 return True
+            elif t<a[m]:
+                h=m-1
+            else:
+                l=m+1
         return False
