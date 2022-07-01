451e62ab4a39f02763b716e003d5d35eefa0c051
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-01 12:39:23 +0530

    Added solution - LeetHub

--- Equilibrium Point - GFG/equilibrium-point.py
+++ Equilibrium Point - GFG/equilibrium-point.py
@@ -6,22 +6,14 @@ class Solution:
     #Function to find equilibrium point in the array.
     def equilibriumPoint(self,A, N):
         # Your code here
-        if len(A)==1:
-            return 1
-            
-        p=[0 for i in range(N) ]
-        s=[0 for i in range(N) ]
-        # print(p,s)
-        for i in range(1,N):
-            p[i]=A[i-1]+p[i-1]
-        # print(p)
-        for i in range(N-2,-1,-1):
-            s[i]=A[i+1]+s[i+1]
-        # print(s)
-        for i in range(N):
-            if s[i]==p[i] and s[i]!=0:
+        tot_sum = sum(A)
+        curr_sum = 0
+        for i in range(len(A)):
+            if curr_sum == (tot_sum- curr_sum-A[i]):
                 return i+1
+            curr_sum+=A[i]
         return -1
+        
         
 
 
