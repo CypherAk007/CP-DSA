6f42ae3b199b051d8d5e3e4dcc01104cc83d33ca
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-02 08:56:02 +0530

    Time: 54 ms (30.18%), Space: 13.9 MB (67.95%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        ans = 1.0
+        nn = n
+        if n<0:
+            nn*=-1
+        while(nn>0):
+            if nn%2==1:
+                ans*=x
+                nn-=1
+            else:
+                x*=x
+                nn=nn//2
+        if n<0:
+            return 1/ans
+        return ans
