651ef7534f1f1d4c06b823548f7ae06c354bfd40
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-14 14:05:05 +0530

    Time: 96 ms (11.14%), Space: 14.1 MB (44.23%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findPeakElement(self, a: List[int]) -> int:
+        # Binary Search
+        l=0
+        h=len(a)-1
+        while(l<h):
+            mid=l+(h-l)//2
+            if a[mid]>a[mid+1]:
+                # you are in desc part of the arr and this may be a potential ans so end!=mid-1
+                # so look for the left side 
+                h=mid
+            else:
+                # you are at the asc part of the arr
+                l=mid+1
+        # if start and end are pointing to same ele that is the ans
+        # at every pt at start and end they have the best possible ans at that time
+        # if only one ele remains thats the max ele
+        return l
