32c682f3588f4307d3c08336f09aeaaeea64b823
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-14 13:59:21 +0530

    Time: 778 ms (5.45%), Space: 27.9 MB (5.41%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,7 +1,20 @@
 class Solution:
     def peakIndexInMountainArray(self, a: List[int]) -> int:
-        # Brute Force
-        for i in range(1,len(a)-1):
-            if a[i]>a[i-1] and a[i]>a[i+1]:
-                return i
+        # Binary Search
+        l=0
+        h=len(a)-1
+        while(l<h):
+            mid=l+(h-l)//2
+            if a[mid]>a[mid+1]:
+                # you are in desc part of the arr and this may be a potential ans so end!=mid-1
+                # so look for the left side 
+                h=mid
+            else:
+                # you are at the asc part of the arr
+                l=mid+1
+        # if start and end are pointing to same ele that is the ans
+        # at every pt at start and end they have the best possible ans at that time
+        # if only one ele remains thats the max ele
+        return l
+            
             
