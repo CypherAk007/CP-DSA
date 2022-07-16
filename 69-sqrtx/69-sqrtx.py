b1bbcf4156f0846ac8c5251e04f1980e5065f205
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-16 17:06:52 +0530

    Time: 61 ms (52.80%), Space: 13.9 MB (56.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,6 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return self.linear(x)
+        # return self.linear(x)
+        return self.bs(x)
     def linear(self,x):
         # sqrt(x)>=y 
         # x>=y*y
@@ -9,3 +10,16 @@ class Solution:
         while(y*y<=x):
             y+=1
         return y-1
+    
+    def bs(self,x):
+        s=0
+        e=x
+        ans=0
+        while(s<=e):
+            m=s+(e-s)//2
+            if m*m<=x:
+                ans=m
+                s=m+1
+            else:
+                e=m-1
+        return ans
