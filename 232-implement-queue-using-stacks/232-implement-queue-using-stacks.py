eacd9812f29a9799d1a7df2b410cfc838e4f5b92
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-07-25 20:01:44 +0530

    Time: 64 ms (8.69%), Space: 14 MB (23.00%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -1,26 +1,34 @@
 class MyQueue:
 
     def __init__(self):
-        self.s1=[]
-        self.s2=[]
+        self.ip=[]
+        self.op=[]
 
     def push(self, x: int) -> None:
-        while(len(self.s1)!=0):
-            val=self.s1.pop()
-            self.s2.append(val)
-        self.s1.append(x)
-        while(len(self.s2)!=0):
-            val=self.s2.pop()
-            self.s1.append(val)
+        self.ip.append(x)
 
     def pop(self) -> int:
-        return self.s1.pop()
+        if len(self.op)!=0:
+            return self.op.pop()
+        else:
+            while(len(self.ip)!=0):
+                val=self.ip.pop()
+                self.op.append(val)
+                
+        return self.op.pop()
 
     def peek(self) -> int:
-        return self.s1[-1]
+        if len(self.op)!=0:
+            return self.op[-1]
+        else:
+            while(len(self.ip)!=0):
+                val=self.ip.pop()
+                self.op.append(val)
+        return self.op[len(self.op)-1]
+        
 
     def empty(self) -> bool:
-        return True if len(self.s1)==0 else False
+        return True if len(self.ip)==0 and len(self.op)==0 else False
 
 
 # Your MyQueue object will be instantiated and called as such:
