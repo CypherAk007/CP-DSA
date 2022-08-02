dcef3057b7fc9fc91cdf415ac1abcb72fa74bb4b
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-02 10:24:21 +0530

    Time: 42 ms (67.96%), Space: 13.8 MB (60.05%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -6,19 +6,40 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        #  Rt l r - Itrative
-        preorder=[]
-        if root==None:
-            return preorder
+        # All in one code
         s=[]
-        s.append(root)
+        s.append([root,1])
+        pre=[]
+        inorder=[]
+        post=[]
+        if root ==None:return 
+        
         while(len(s)!=0):
-            root=s.pop()
-            preorder.append(root.val)
-            if root.right !=None:
-                s.append(root.right)
-            if root.left!=None:
-                s.append(root.left)
-        return preorder
+            it = s.pop()
+            # this is part of pre
+            # we inc 1 to 2
+            # pushthe left side of the tree
+            if (it[1]==1):
+                pre.append(it[0].val)
+                it[1]+=1
+                s.append(it)
+                
+                if it[0].left!=None:
+                    s.append([it[0].left,1])
+            # this is part of in
+            # we inc 2 to 3
+            # pushthe right side of the tree  
+            elif (it[1]==2):
+                inorder.append(it[0].val)
+                it[1]+=1
+                s.append(it)
+                
+                if it[0].right!=None:
+                    s.append([it[0].right,1])
+            # dont push it back again    
+            else:
+                
+                post.append(it[0].val)
+        return pre
         
         
