67e051ddcdcd5eda350fb0e96d72bc596cb5ea1b
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-02 16:28:32 +0530

    Time: 33 ms (90.82%), Space: 13.8 MB (98.38%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,7 +6,22 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # M-1
         # we do preorder traversal simultaneously on both the trees
-        if p==None or q==None:
-            return p==q
-        return (p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
+        # if p==None or q==None:
+        #     return p==q
+        # return (p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right))
+        
+        # M-2 do preorder traversal and compare the returned arrays
+        pTree=self.preorder(p)
+        qTree=self.preorder(q)
+        # print(pTree,qTree)
+        return pTree==qTree
+    def preorder(self,root):
+        if root==None:
+            return [None]
+        lst=[]
+        lst.append(root.val)
+        lst+=self.preorder(root.left)
+        lst+=self.preorder(root.right)
+        return lst
