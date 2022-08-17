2229d6a9c5aff96c45e3c54272a354531c5cf9a3
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-18 01:28:45 +0530

    Time: 56 ms (32.89%), Space: 13.8 MB (66.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,14 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp=[-1]*(len(nums)-1+1)
-        return self.maxsum(len(nums)-1,nums,dp)
-    def maxsum(self,n,a,dp):
-        dp[0]=a[0]
-        for idx in range(1,n+1):
-            pick=a[idx]
-            if idx>1:
-                pick+=dp[idx-2]
-            notp=0+dp[idx-1]
-            dp[idx]=max(pick,notp)
-        return dp[n]
+        n=len(nums)
+        prev=nums[0]
+        prev2=0
+        for i in range(1,n):
+            pick=nums[i]
+            if i>1:
+                pick+=prev2
+            notp=0+prev
+            curi=max(pick,notp)
+            prev2=prev
+            prev=curi
+        return prev
         
