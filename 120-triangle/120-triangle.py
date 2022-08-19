34d8f81c71b0022810110df95f9ad7b6b5643f16
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-19 16:00:34 +0530

    Time: 99 ms (62.55%), Space: 15.3 MB (23.87%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -2,16 +2,19 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n=len(triangle)
         dp=[[-1]*n for i in range(n)]
-        return self.helper(0,0,triangle,n,dp)
+        return self.helper(triangle,n,dp)
     
-    # Simple Recursion
-    def helper(self,i,j,triangle,n,dp):
-        if i==n-1:
-            return triangle[i][j]
-        if dp[i][j]!=-1:
-            return dp[i][j]
-        down=triangle[i][j]+self.helper(i+1,j,triangle,n,dp)
-        diag=triangle[i][j]+self.helper(i+1,j+1,triangle,n,dp)
-        dp[i][j]=min(down,diag)
-        return dp[i][j]
+    # Tabulation
+    def helper(self,triangle,n,dp):
+        # base case
+        for j in range(n):
+            dp[n-1][j]=triangle[n-1][j]
+        
+        for i in range(n-2,-1,-1):
+            for j in range(i,-1,-1):
+                down=triangle[i][j]+dp[i+1][j]
+                diag=triangle[i][j]+dp[i+1][j+1]
+                dp[i][j]=min(down,diag)
+
+        return dp[0][0]
     
