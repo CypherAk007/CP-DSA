46ac52204e54ecef4ceeebdc489357f86fea3449
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-19 11:49:45 +0530

    Time: 61 ms (63.33%), Space: 14.1 MB (43.62%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -2,21 +2,23 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         r=len(obstacleGrid)
         c=len(obstacleGrid[0])
-        dp=[[-1]*c for i in range(r)]
+        dp=[[0]*c for i in range(r)]
         return self.helper(r-1,c-1,obstacleGrid,dp)
-    # Basic recursion
+    # Tabulation
     def helper(self,r,c,obstacleGrid,dp):
-        if obstacleGrid[r][c]==1:
-            return 0
-        if r==0 and c==0:
-            return 1
-        if dp[r][c]!=-1:
-            return dp[r][c]
-        up=left=0
-        if r>0:
-            up=self.helper(r-1,c,obstacleGrid,dp)
-        if c>0:
-            left=self.helper(r,c-1,obstacleGrid,dp)
-        dp[r][c]=up+left
+        for i in range(0,r+1):
+            for j in range(0,c+1):
+                if obstacleGrid[i][j]==1:
+                    dp[i][j]=0
+                else:
+                    if i==0 and j==0:
+                        dp[i][j]=1
+                    else:
+                        up=left=0
+                        if i>0:
+                            up=dp[i-1][j]
+                        if j>0:
+                            left=dp[i][j-1]
+                        dp[i][j]=up+left
         return dp[r][c]
             
