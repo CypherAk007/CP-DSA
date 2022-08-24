c7cf55edb4d74a62945827656f0d9bd7f512ad72
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-24 21:41:46 +0530

    Time: 844 ms (31.75%), Space: 34.9 MB (28.51%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,10 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        dp=[[-1]*(amount+1) for i in range(n)]
-        return self.helper(n-1,amount,coins,dp)
+        # dp=[[-1]*(amount+1) for i in range(n)]
+        # return self.helper(n-1,amount,coins,dp)
+        dp=[[0]*(amount+1) for i in range(n)]
+        return self.tabhelper(amount,coins,dp,n)
     
-    #SIMPLE RECURSION
+    #SIMPLE RECURSION + MEMOIZATION
     def helper(self,idx,t,coins,dp):
         if idx==0:
             if t%coins[0]==0:
@@ -20,3 +22,20 @@ class Solution:
         dp[idx][t]=np+p
         return dp[idx][t]
     
+    #Tabulation
+    def tabhelper(self,target,coins,dp,n):
+        for T in range(0,target+1):
+            if T%coins[0]==0:
+                dp[0][T]=1
+            else:
+                dp[0][T]=0
+        
+        for idx in range(1,n):
+            for t in range(0,target+1):
+                np=dp[idx-1][t]
+                p=0
+                if coins[idx]<=t:
+                    p=dp[idx][t-coins[idx]]
+                dp[idx][t]=np+p
+                
+        return dp[n-1][target]
