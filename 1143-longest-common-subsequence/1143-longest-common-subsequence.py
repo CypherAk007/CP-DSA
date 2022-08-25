88500286ad88e1c6d662f9201668d5632dc86fb1
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-25 18:34:33 +0530

    Time: 1511 ms (13.53%), Space: 25.3 MB (25.66%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n=len(text1)
+        m=len(text2)
+        dp=[[-1]*(m) for i in range(n)]
+        return self.helper(n-1,m-1,text1,text2,dp)
+    
+    # Simple recursion
+    def helper(self,idx1,idx2,text1,text2,dp):
+        if idx1<0 or idx2<0:
+            return 0
+        
+        if dp[idx1][idx2]!=-1:
+            return dp[idx1][idx2]
+        
+        if text1[idx1]==text2[idx2]:
+            dp[idx1][idx2]=1 + self.helper(idx1-1,idx2-1,text1,text2,dp)
+            return dp[idx1][idx2]
+        
+        dp[idx1][idx2]=0+ max(self.helper(idx1-1,idx2,text1,text2,dp),self.helper(idx1,idx2-1,text1,text2,dp))
+        return dp[idx1][idx2]
+    
+    
