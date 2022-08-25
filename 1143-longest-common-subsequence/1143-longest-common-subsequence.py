601a06c9dcf771a1736dcf43664707e9a29d7ff7
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-25 18:48:02 +0530

    Time: 1321 ms (17.20%), Space: 25.6 MB (25.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,18 +2,23 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n=len(text1)
         m=len(text2)
-        dp=[[-1]*(m) for i in range(n)]
-        return self.helper(n-1,m-1,text1,text2,dp)
+        
+        # dp=[[-1]*(m) for i in range(n)]
+        dp=[[-1]*(m+1) for i in range(n+1)]
+        
+        # return self.helper(n-1,m-1,text1,text2,dp) replace n-1 and m-1 with n,m(shifting indexes)
+        return self.helper(n,m,text1,text2,dp)
     
-    # Simple recursion
+    # i===i-1 and j===j-1
+    # Tabulation
     def helper(self,idx1,idx2,text1,text2,dp):
-        if idx1<0 or idx2<0:
+        if idx1==0 or idx2==0: # idx shifted so -1 is treated as 0
             return 0
         
         if dp[idx1][idx2]!=-1:
             return dp[idx1][idx2]
         
-        if text1[idx1]==text2[idx2]:
+        if text1[idx1-1]==text2[idx2-1]:
             dp[idx1][idx2]=1 + self.helper(idx1-1,idx2-1,text1,text2,dp)
             return dp[idx1][idx2]
         
