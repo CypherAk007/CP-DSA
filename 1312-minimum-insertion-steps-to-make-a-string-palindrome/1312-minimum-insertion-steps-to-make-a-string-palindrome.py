7d8e8c48ffc036e8e8700a3b8fc2bcfcdc67ecef
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-26 10:31:44 +0530

    Time: 2354 ms (24.34%), Space: 17 MB (33.42%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        return len(s)-self.longestPalindromeSubseq(s)
+        
+    def longestPalindromeSubseq(self, s: str) -> int:
+        s1=s
+        s2=s[::-1]
+        return self.longestCommonSubsequence(s1,s2)
+        
+    def longestCommonSubsequence(self, text1, text2):
+        n=len(text1)
+        m=len(text2)
+        dp=[[-1]*(m) for i in range(n)]
+        return self.helper(n-1,m-1,text1,text2,dp)
+    
+    # Simple recursion
+    def helper(self,idx1,idx2,text1,text2,dp):
+        if idx1<0 or idx2<0:
+            return 0
+        
+        if dp[idx1][idx2]!=-1:
+            return dp[idx1][idx2]
+        
+        if text1[idx1]==text2[idx2]:
+            dp[idx1][idx2]=1 + self.helper(idx1-1,idx2-1,text1,text2,dp)
+            return dp[idx1][idx2]
+        
+        dp[idx1][idx2]=0+ max(self.helper(idx1-1,idx2,text1,text2,dp),self.helper(idx1,idx2-1,text1,text2,dp))
+        return dp[idx1][idx2]
