ebdae31206028cf1a0f811b9bee542423e4022d1
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-26 19:20:59 +0530

    Time: 931 ms (54.27%), Space: 70.7 MB (51.54%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -2,9 +2,16 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         n=len(s)
         m=len(t)
+        
         # return self.helperRecursion(n-1,m-1,s,t)
-        dp=[[-1]*m for i in range(n)]
-        return self.helperMemoization(n-1,m-1,s,t,dp)
+        
+        # dp=[[-1]*m for i in range(n)]
+        # return self.helperMemoization(n-1,m-1,s,t,dp)
+        
+        # dp=[[-1]*(m+1) for i in range(n+1)]
+        # return self.helperMemoization1(n,m,s,t,dp)
+    
+        return self.helperTab(s,t)
     
     def helperRecursion(self,i,j,s,t):
         # bc
@@ -18,19 +25,43 @@ class Solution:
         
         return self.helperRecursion(i-1,j,s,t)
         
-    def helperMemoization(self,i,j,s,t,dp):
+    
+    def helperMemoization1(self,i,j,s,t,dp): #converted to 1-based indexing
         # bc
-        if j<0:
+        if j==0:
             return 1
-        if i<0:
+        if i==0:
             return 0
         if dp[i][j]!=-1:
             return dp[i][j]
         
-        if s[i]==t[j]:
-            dp[i][j]=self.helperMemoization(i-1,j-1,s,t,dp)+self.helperMemoization(i-1,j,s,t,dp)
+        if s[i-1]==t[j-1]:
+            dp[i][j]=self.helperMemoization1(i-1,j-1,s,t,dp)+self.helperMemoization1(i-1,j,s,t,dp)
             return dp[i][j]
         
-        dp[i][j]=self.helperMemoization(i-1,j,s,t,dp)
+        dp[i][j]=self.helperMemoization1(i-1,j,s,t,dp)
         return dp[i][j]
+    
+    def helperTab(self,s,t):
+        n=len(s)
+        m=len(t)
+        dp=[[0]*(m+1) for i in range(n+1)]
+        # BC
+        for i in range(0,n+1):
+            dp[i][0]=1 
+        # fm 1->m+1 cause ith loop runs fm dp[0][0]=1 and jth also dp[0][0]=0 overlap
+        for j in range(1,m+1): 
+            dp[0][j]=0
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[n][m]
+    
+        
+        
+    
         
