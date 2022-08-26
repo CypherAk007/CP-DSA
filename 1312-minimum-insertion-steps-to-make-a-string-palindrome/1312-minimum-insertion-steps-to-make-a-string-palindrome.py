0f83f9c145a4aa3a543682952388d7ea9991d080
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-26 10:32:51 +0530

    Time: 1399 ms (64.44%), Space: 17.8 MB (29.89%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -10,20 +10,29 @@ class Solution:
     def longestCommonSubsequence(self, text1, text2):
         n=len(text1)
         m=len(text2)
-        dp=[[-1]*(m) for i in range(n)]
-        return self.helper(n-1,m-1,text1,text2,dp)
+        
+        # dp=[[-1]*(m) for i in range(n)]
+        dp=[[0]*(m+1) for i in range(n+1)]
+        
+        # return self.helper(n-1,m-1,text1,text2,dp) replace n-1 and m-1 with n,m(shifting indexes)
+        return self.helper(text1,text2,dp)
     
-    # Simple recursion
-    def helper(self,idx1,idx2,text1,text2,dp):
-        if idx1<0 or idx2<0:
-            return 0
+    # i===i-1 and j===j-1
+    # Tabulation
+    def helper(self,text1,text2,dp):
+        # bc
+        n=len(text1)
+        m=len(text2)
+        for j in range(0,m+1):
+            dp[0][j]=0
+        for i in range(0,n+1):
+            dp[i][0]=0
+            
+        for idx1 in range(1,n+1):
+            for idx2 in range(1,m+1):
+                if text1[idx1-1]==text2[idx2-1]:
+                    dp[idx1][idx2]=1 + dp[idx1-1][idx2-1]
+                else:
+                    dp[idx1][idx2]=0+ max(dp[idx1-1][idx2],dp[idx1][idx2-1])
         
-        if dp[idx1][idx2]!=-1:
-            return dp[idx1][idx2]
-        
-        if text1[idx1]==text2[idx2]:
-            dp[idx1][idx2]=1 + self.helper(idx1-1,idx2-1,text1,text2,dp)
-            return dp[idx1][idx2]
-        
-        dp[idx1][idx2]=0+ max(self.helper(idx1-1,idx2,text1,text2,dp),self.helper(idx1,idx2-1,text1,text2,dp))
         return dp[idx1][idx2]
