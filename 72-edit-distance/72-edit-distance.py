4a217402f4607d6d21adef6b971746dc2621671f
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-28 09:01:04 +0530

    Time: 281 ms (37.74%), Space: 19.9 MB (13.03%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -3,7 +3,8 @@ class Solution:
         n=len(word1)
         m=len(word2)
         dp=[[-1]*(m+1)for i in range(n+1)]
-        return self.helper(n,m,word1,word2,dp)
+        # return self.helper(n,m,word1,word2,dp)
+        return self.helperTab(word1,word2)
     
     # Recursion + memoization  (0-BASED ===> 1-BASED)
     def helper(self,i,j,s1,s2,dp):
@@ -25,3 +26,26 @@ class Solution:
             c3=1+self.helper(i-1,j-1,s1,s2,dp) #replace
         dp[i][j]=min(c1,min(c2,c3))
         return dp[i][j]
+    
+    # Tabulation
+    def helperTab(self,s1,s2):
+        n=len(s1)
+        m=len(s2)
+        dp=[[0]*(m+1)for i in range(n+1)]
+        
+        # bc
+        for i in range(n+1):
+            dp[i][0]=i
+        for j in range(m+1):
+            dp[0][j]=j
+            
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=0+dp[i-1][j-1]
+                else:
+                    dp[i][j]= 1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]))
+                
+        return dp[n][m]
+    
+    
