b7fcf31e331687b2b7e65c4a22bc85b4c12a16ff
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-29 19:18:23 +0530

    Time: 3843 ms (22.75%), Space: 91.6 MB (15.35%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -3,24 +3,25 @@ class Solution:
         n=len(prices)
         dp=[[[-1]*(3) for i in range(2)]for j in range(n)]
         # print(dp)
-        return self.helper(0,1,2,prices,n,dp) # [idx,buy,cap,prices]
+        return self.helper(prices,n,) # [idx,buy,cap,prices]
     
-    # RECURSION
-    def helper(self,idx,buy,cap,prices,n,dp):
-        # bc
-        if cap==0:
-            return 0
-        if idx==n:
-            return 0
+    # Tabulation
+    def helper(self,prices,n,):
+        dp=[[[0]*(3) for i in range(2)]for j in range(n+1)]
+        #   NO NEED TO WRITE bc AS ALL THE VAL INIT ARE 0
+        # if cap==0:
+        #     return 0
+        # if idx==n:
+        #     return 0
         
-        if dp[idx][buy][cap]!=-1:
-            return dp[idx][buy][cap]
-        
-        if buy:
-            dp[idx][buy][cap]=max(-prices[idx]+self.helper(idx+1,0,cap,prices,n,dp),
-                      0+self.helper(idx+1,1,cap,prices,n,dp))
-            return dp[idx][buy][cap]
-        else:
-            dp[idx][buy][cap]=max(prices[idx]+self.helper(idx+1,1,cap-1,prices,n,dp),
-                      0+self.helper(idx+1,0,cap,prices,n,dp))
-            return dp[idx][buy][cap]
+        for idx in range(n-1,-1,-1):
+            for buy in range(0,2):
+                for cap in range(1,3): #cap starts fm 1 as in bc- cap==0: return 0
+                    if buy==1:
+                        dp[idx][buy][cap]=max(-prices[idx]+dp[idx+1][0][cap],
+                                  0+dp[idx+1][1][cap])
+                        
+                    else:
+                        dp[idx][buy][cap]=max(prices[idx]+dp[idx+1][1][cap-1],
+                                  0+dp[idx+1][0][cap])
+        return dp[0][1][2]
