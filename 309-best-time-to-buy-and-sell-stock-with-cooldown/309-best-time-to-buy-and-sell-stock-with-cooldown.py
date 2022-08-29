6bb764d3f666a7435e47dbaf568f1ae94eb774a9
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-29 21:50:06 +0530

    Time: 90 ms (23.27%), Space: 14.7 MB (50.18%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,23 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[-1]*(3)for i in range(n)]
-        return self.helper(0,1,prices,n,dp)
+        dp=[[0]*(2)for i in range(n+2)]
+        return self.helper(prices,n,dp)
     
-    # RECURSION + Memo
-    def helper(self,idx,buy,prices,n,dp):
+    # Tabulation
+    def helper(self,prices,n,dp):
         # bc
-        if idx>=n:
-            return 0
-        
-        if dp[idx][buy]!=-1:
-            return dp[idx][buy]
-        
-        if buy:
-            dp[idx][buy]=max(-prices[idx]+self.helper(idx+1,0,prices,n,dp),
-                        0+self.helper(idx+1,1,prices,n,dp))
-            return dp[idx][buy]
-        else:
-            dp[idx][buy]=max(prices[idx]+self.helper(idx+2,1,prices,n,dp),
-                      0+self.helper(idx+1,0,prices,n,dp))
-            return dp[idx][buy]
+        dp[n][0]=dp[n][1]=0
+        for idx in range(n-1,-1,-1):
+            for buy in range(0,2):
+                profit=0
+                if buy:
+                    profit=max(-prices[idx]+dp[idx+1][0],
+                                0+dp[idx+1][1])
+                else:
+                    profit=max(prices[idx]+dp[idx+2][1],
+                              0+dp[idx+1][0])
+                dp[idx][buy]=profit
+        return dp[0][1]
