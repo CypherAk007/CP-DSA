cf9fd5e797ea7ce6fa9d4bcdf96d98631a0aedc6
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-08-29 19:31:42 +0530

    Time: 3752 ms (23.52%), Space: 49.5 MB (25.41%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -3,25 +3,22 @@ class Solution:
         n=len(prices)
         dp=[[[-1]*(3) for i in range(2)]for j in range(n)]
         # print(dp)
-        return self.helper(prices,n,) # [idx,buy,cap,prices]
+        return self.helper(prices,n) # [idx,buy,cap,prices]
     
-    # Tabulation
-    def helper(self,prices,n,):
-        dp=[[[0]*(3) for i in range(2)]for j in range(n+1)]
-        #   NO NEED TO WRITE bc AS ALL THE VAL INIT ARE 0
-        # if cap==0:
-        #     return 0
-        # if idx==n:
-        #     return 0
+    # Tabulation+ space
+    def helper(self,prices,n):
+        after=[[0]*(3)for i in range(2)]
+        cur=[[0]*(3)for i in range(2)]
         
         for idx in range(n-1,-1,-1):
             for buy in range(0,2):
                 for cap in range(1,3): #cap starts fm 1 as in bc- cap==0: return 0
                     if buy==1:
-                        dp[idx][buy][cap]=max(-prices[idx]+dp[idx+1][0][cap],
-                                  0+dp[idx+1][1][cap])
+                        cur[buy][cap]=max(-prices[idx]+after[0][cap],
+                                  0+after[1][cap])
                         
                     else:
-                        dp[idx][buy][cap]=max(prices[idx]+dp[idx+1][1][cap-1],
-                                  0+dp[idx+1][0][cap])
-        return dp[0][1][2]
+                        cur[buy][cap]=max(prices[idx]+after[1][cap-1],
+                                  0+after[0][cap])
+            after=cur
+        return after[1][2]
