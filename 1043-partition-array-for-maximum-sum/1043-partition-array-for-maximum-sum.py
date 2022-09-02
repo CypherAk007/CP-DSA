a451f760e826db7215e3f1ee577415d0c3b5ceb7
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-09-02 17:53:51 +0530

    Time: 512 ms (93.35%), Space: 14 MB (70.52%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -2,7 +2,9 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n=len(arr)
         dp=[-1]*(n)
-        return self.helper(0,arr,n,k,dp)
+        # return self.helper(0,arr,n,k,dp)
+        
+        return self.helperTab(arr,k)
     
     def helper(self,idx,arr,n,k,dp):
         if idx==n:
@@ -19,3 +21,18 @@ class Solution:
             maxAns=max(maxAns,summ)
         dp[idx]=maxAns
         return dp[idx]
+    
+    def helperTab(self,arr,k):
+        n=len(arr)
+        dp=[0]*(n+1)
+        for idx in range(n-1,-1,-1):
+            maxAns=float('-inf')
+            maxi=float('-inf')
+            length=0
+            for j in range(idx,min(n,idx+k)):
+                length+=1
+                maxi=max(maxi,arr[j])
+                summ=(length*maxi)+dp[j+1]
+                maxAns=max(maxAns,summ)
+            dp[idx]=maxAns
+        return dp[0]
