4bbd00a55646265487243ec5faa1646fbc518a52
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-09-17 07:50:43 +0530

    Time: 973 ms (5.02%), Space: 16.5 MB (48.08%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,14 +3,16 @@ class Solution:
         n=len(grid)
         m=len(grid[0])
         vis=[[0]*m for i in range(n)]
-
+        dr=[-1,0,1,0]
+        dc=[0,1,0,-1]
         cnt=0
         for row in range(0,n):
             for col in range(0,m):
                 if (vis[row][col]==0 and grid[row][col]=='1'):
                     
                     cnt+=1
-                    self.bfs(row,col,vis,grid)
+                    # self.bfs(row,col,vis,grid)
+                    self.dfs(row,col,vis,grid,n,m,dr,dc)
                     
         return cnt
     
@@ -34,6 +36,14 @@ class Solution:
                         if nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol] == '1' and vis[nrow][ncol]==0:
                             vis[nrow][ncol]=1
                             q.append([nrow,ncol])
+    
+    def dfs(self,row,col,vis,grid,n,m,dr,dc):
+        vis[row][col]=1
+        for i in range(4):
+            nr=row+dr[i]
+            nc=col+dc[i]
+            if nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc]=='1' and vis[nr][nc]==0:
+                self.dfs(nr,nc,vis,grid,n,m,dr,dc)
             
                         
                     
