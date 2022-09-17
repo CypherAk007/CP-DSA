1fa3dc5806f80d46734edbee10101f30afcbacb5
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-09-17 08:02:20 +0530

    Time: 199 ms (5.01%), Space: 14.1 MB (38.00%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -4,7 +4,10 @@ class Solution:
         ans=image
         delRow=[-1,0,+1,0]
         delCol=[0,+1,0,-1]
-        self.dfs(sr,sc,image,color,delRow,delCol,iniColor,ans)
+        n=len(image)
+        m=len(image[0])
+        # self.dfs(sr,sc,image,color,delRow,delCol,iniColor,ans)
+        self.bfs(sr,sc,ans,image,delRow,delCol,n,m,color,iniColor)
         return ans
     
     def dfs(self,row,col,image,color,delRow,delCol,iniColor,ans):
@@ -17,4 +20,20 @@ class Solution:
             if nrow>=0 and nrow<n and ncol>=0 and ncol<m and image[nrow][ncol] == iniColor and ans[nrow][ncol] !=color:
                 self.dfs(nrow,ncol,image,color,delRow,delCol,iniColor,ans)
                 
+    def bfs(self,row,col,ans,image,dr,dc,n,m,color,initcolor):
+        q=[]
+        q.append([row,col])
+        ans[row][col]=color
+        while(q):
+            node=q.pop(0)
+            r=node[0]
+            c=node[1]
+            for i in range(4):
+                nr=r+dr[i]
+                nc=c+dc[i]
+                if nr>=0 and nr<n and nc>=0 and nc<m and image[nr][nc]==initcolor and ans[nr][nc]!=color:
+                    q.append([nr,nc])
+                    ans[nr][nc]=color
+                    
+            
         
