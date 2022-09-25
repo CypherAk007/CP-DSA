c423f18ae1c14f9a7bc3b40c7c5f2a70e8aca93a
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-09-25 18:20:46 +0530

    Time: 375 ms (48.73%), Space: 15.4 MB (64.56%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,44 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        q=[]
+        q.append([list(beginWord),1]) #word,level
+        d={}
+        wl=[]
+        for i in wordList:
+            wl.append(tuple(i))
+
+        
+        for i in wl:
+            d[i]=0
+
+        while(len(q)!=0):
+
+            n=q.pop(0)
+            word=n[0]
+            steps=n[1]
+            
+            if word==list(endWord):
+                return steps
+            
+            # now we have to change every char in the word
+            # word=hat
+            
+            for i in range(0,len(word)):
+                # 0th char -h
+
+                original=word[i] #char that we are looking to change
+                for ch in range(ord('a'),ord('z')+1):
+                    # 0th got replaced
+                    # aat , bat ,cat,...zat
+                    word[i]=chr(ch)
+
+                    # if the word exists in the dict and not visited
+                    if tuple(word) in d and d[tuple(word)]!=1:
+                        d.pop(tuple(word))
+                        x=word[:]
+                        q.append([x,steps+1])
+
+                        
+                word[i]=original
+
+        return 0 
