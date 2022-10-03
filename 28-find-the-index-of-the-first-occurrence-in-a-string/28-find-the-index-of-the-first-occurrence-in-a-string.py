70ec87ca6df21195b9fb7200a0a74cba9696b91a
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-10-03 14:11:10 +0530

    Time: 40 ms (76.77%), Space: 13.9 MB (17.07%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,35 +1,21 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # KMP ALGO.
         if len(needle)==0:
             return 0
-        # making of lps
-        lps=[0]*len(needle)
-        prevlps=0
-        i=1
-        while i<len(needle):
-            if needle[i]==needle[prevlps]:
-                lps[i]=prevlps + 1
-                prevlps+=1
-                i+=1
-            else:
-                if prevlps==0:
-                    lps[i]=0
-                    i+=1
-                else:
-                    prevlps=lps[prevlps-1]
-        
         i=0
         j=0
-        while i<len(haystack):
-            if haystack[i]==needle[j]:
+        k=len(needle)
+        temp=''
+        while j<len(haystack):
+            temp+=haystack[j]
+            
+            if j-i+1<k:
+                j+=1
+                
+            elif j-i+1==k:
+                if temp==needle:
+                    return i
+                temp=temp[1:]
                 i+=1
                 j+=1
-            else:
-                if j==0:
-                    i+=1
-                else:
-                    j=lps[j-1]
-            if j==len(needle):
-                return i-len(needle)
         return -1
