fff93acac3b27c227628f03dd540a977d097a15c
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-10-17 15:36:25 +0530

    Added solution - LeetHub

---
+++ Wildcard string matching - GFG/wildcard-string-matching.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    def match(self, wild, pattern):
+        # code here
+        # recursive sol
+        return self.helper(len(wild)-1,len(pattern)-1,wild,pattern)
+    def helper(self,i,j,s1,s2):
+        # bc
+        if i<0 and j<0:
+            return True
+        if i<0 and j>=0:
+            return False
+        if i>=0 and j<0:
+            for k in range(0,i):
+                if s1[k]=='*':
+                    return True
+                else:
+                    return False
+        
+        if s1[i]==s2[j] or s1[i]=='?':
+            return self.helper(i-1,j-1,s1,s2)
+        
+        if s1[i]=='*':
+            return self.helper(i-1,j,s1,s2) or self.helper(i,j-1,s1,s2)
+        
+        return False
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range (t):
+        wild = input()
+        pattern = input()
+        
+        ob = Solution()
+        if(ob.match(wild, pattern)):
+            print("Yes")
+        else:
+            print("No")
+# } Driver Code Ends
