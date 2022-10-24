7bc7301dfdd962e01b81ae9c9c41b451ff499f59
CypherAk007 <71595919+CypherAk007@users.noreply.github.com>
2022-10-24 15:50:06 +0530

    Time: 124 ms (41.18%), Space: 14.1 MB (12.82%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,11 +1,17 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        t=n
-        c=0
-        for i in range(1,n+1):
-            if i<=t:
-                t-=i
-                c+=1 
-            if i>t:
-                break
-        return c
+        return self.bs(0,n,n)
+    def bs(self,lo,hi,n):
+        res=-1
+        while(lo<=hi):
+            mid=lo+(hi-lo)//2
+            x=(mid*(mid+1))//2
+            print(lo,hi,mid,x,res)
+            if x==n:
+                return mid
+            elif x>n:
+                hi=mid-1
+            else:
+                res=mid
+                lo=mid+1
+        return res
